a75fa0622a38cc5ccc273a5cff273b0902042d70
anlcan <anilcanbaykal@gmail.com>
2022-01-08 16:07:49 +0100

    day 5.2

--- src/main/java/com/acb/dayFive/ThermalMap.java
+++ src/main/java/com/acb/dayFive/ThermalMap.java
@@ -7,8 +7,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static java.lang.Math.max;
-import static java.lang.Math.min;
+import static java.lang.Math.*;
 
 /**
  * Created on 31.12.21.
@@ -20,34 +19,45 @@ public class ThermalMap {
         public static Point of(final String str){
             return new Point(Integer.parseInt(str.split(",")[0]),Integer.parseInt(str.split(",")[1]));
         }
-        public static List<Point> inBetween(Point a, Point b){
-            //            only consider horizontal and vertical lines:
-            if (a.x != b.x && a.y != b.y) {
-                return new ArrayList<>();
-            }
+
+
+        public static List<Point> inBetweenStraight(Point a, Point b){
+
             if (a.x == b.x){
                 return IntStream.rangeClosed(min(a.y, b.y), max(a.y, b.y))
                         .boxed()
                         .map(i -> new Point(a.x, i))
                         .collect(Collectors.toList());
-            } else {
+            } else if (a.y == b.y) {
                 return IntStream.rangeClosed(min(a.x, b.x), max(a.x, b.x))
                         .boxed()
                         .map(i -> new Point(i, a.y))
                         .collect(Collectors.toList());
+            } else {
+                return new ArrayList<>();
+            }
+        }
+
+        public static List<Point> inBetweenDiagonal(Point a, Point b){
+            if ( abs(a.x - b.x) == abs(a.y - b.y)) {
+                return IntStream.rangeClosed(0, abs(a.x - b.x))
+                        .boxed()
+                        .map(i -> new Point(a.x + i, a.y + i))
+                        .collect(Collectors.toList());
+            } else {
+                return new ArrayList<>();
             }
         }
     }
+
     public static final class Coordinate{
         final Point p;
         final int value;
         public Coordinate(Coordinate coordinate){
-            this.p = new Point(coordinate.p.x, coordinate.p.y);
-            this.value = coordinate.value+1;
+            this(new Point(coordinate.p.x, coordinate.p.y),coordinate.value+1);
         }
         public Coordinate (Point p){
-            this.p = new Point(p.x, p.y);
-            this.value = 1;
+            this(p,1);
         }
 
         public Coordinate(Point p, int i) {
@@ -58,17 +68,40 @@ public class ThermalMap {
 
     final Map<Point, Coordinate> coordinates;
 
-    public static List<Point> parseCommand(final String str){
+    private static List<Point> readCommand(final String str){
         final String[] split = str.split("->");
         assert split.length == 2;
-        return Point.inBetween(Point.of(split[0].trim()), Point.of(split[1].trim()));
+        return List.of(Point.of(split[0].trim()), Point.of(split[1].trim()));
     }
 
+    public static List<Point> parseCommand(final String str){
+        final List<Point> points = readCommand(str);
+        return Point.inBetweenStraight(points.get(0), points.get(1));
+    }
 
-    public static List<List<Point>> read(final List<String> commands){
+    public static List<Point> parseCommandAll(final String str){
+        final List<Point> points = readCommand(str);
+
+        final List<Point> path = Point.inBetweenStraight(points.get(0), points.get(1));
+        if (path.size() == 0) {
+            final List<Point> path2 = Point.inBetweenDiagonal(points.get(0), points.get(1));
+            if (path2.size() ==0 ){
+                throw new RuntimeException(str);
+            }
+            return path2;
+        } else {
+            return path;
+        }
+    }
+
+    public static List<List<Point>> readStraight(final List<String> commands){
         return commands.stream().map(ThermalMap::parseCommand)
-                        .filter(p-> p.size()>0)
-                        .collect(Collectors.toList());
+                .collect(Collectors.toList());
+    }
+
+    public static List<List<Point>> readAll(final List<String> commands) {
+        return commands.stream().map(ThermalMap::parseCommandAll)
+                .collect(Collectors.toList());
     }
 
     public ThermalMap(List<List<Point>> pointsList){
@@ -87,4 +120,5 @@ public class ThermalMap {
     public long countOverLappedPoints(){
         return coordinates.values().stream().filter(c -> c.value >= 2).count();
     }
+
 }
--- src/test/java/com/acb/advent2021/DayFive.java
+++ src/test/java/com/acb/advent2021/DayFive.java
@@ -16,11 +16,20 @@ public class DayFive {
 
     @Test
     public void inBetweenTest(){
-        final List<ThermalMap.Point> points = ThermalMap.Point.inBetween(new ThermalMap.Point(1, 1), new ThermalMap.Point(1, 3));
+        final List<ThermalMap.Point> points = ThermalMap.Point.inBetweenStraight(new ThermalMap.Point(1, 1), new ThermalMap.Point(1, 3));
         assertEquals(3, points.size());
 
+        final List<ThermalMap.Point> diagonalPoints = ThermalMap.Point.inBetweenDiagonal(new ThermalMap.Point(1, 1), new ThermalMap.Point(3, 3));
+        assertEquals(3, diagonalPoints.size());
+
+        final List<ThermalMap.Point> diagonalPoints2 = ThermalMap.parseCommandAll("9,7 -> 7,9");
+        assertEquals(3, diagonalPoints2.size());
+
+        final List<ThermalMap.Point> diagonalPoints3 = ThermalMap.parseCommandAll("8,0 -> 0,8");
+        assertEquals(9, diagonalPoints3.size());
+
         final List<ThermalMap.Point> commands = ThermalMap.parseCommand("1,1 -> 1,3");
-        assertEquals(3, points.size());
+        assertEquals(3, commands.size());
 
         assertEquals(new ThermalMap.Point(1,1), new ThermalMap.Point(1,1));
         assertNotEquals(new ThermalMap.Point(1,2), new ThermalMap.Point(1,1));
@@ -29,11 +38,10 @@ public class DayFive {
 
     @Test
     public void intersectionTest() {
-        final List<ThermalMap.Point> points1 = ThermalMap.Point.inBetween(new ThermalMap.Point(1, 1), new ThermalMap.Point(1, 3));
+        final List<ThermalMap.Point> points1 = ThermalMap.Point.inBetweenStraight(new ThermalMap.Point(1, 1), new ThermalMap.Point(1, 3));
         final List<ThermalMap.Point> point2 = ThermalMap.parseCommand("0,2 -> 2,2");
 
         final List<List<ThermalMap.Point>> points11 = List.of(points1, point2);
-        System.out.println(points11);
         final ThermalMap thermalMap = new ThermalMap(points11);
         assertEquals(1, thermalMap.countOverLappedPoints());
 
@@ -41,16 +49,28 @@ public class DayFive {
     @Test
     public void sampleTest(){
         final List<String> strings = Util.readStrings("/day5/sample.txt");
-        final List<List<ThermalMap.Point>> commands = ThermalMap.read(strings);
+        final List<List<ThermalMap.Point>> commands = ThermalMap.readStraight(strings);
         ThermalMap tm = new ThermalMap(commands);
         assertEquals(5, tm.countOverLappedPoints());
+
+        final List<List<ThermalMap.Point>> commandsAll = ThermalMap.readAll(strings);
+        ThermalMap tm2 = new ThermalMap(commandsAll);
+        assertEquals(12, tm2.countOverLappedPoints());
     }
 
     @Test
     public void inputOneTest(){
         final List<String> strings = Util.readStrings("/day5/input1.txt");
-        final List<List<ThermalMap.Point>> commands = ThermalMap.read(strings);
+        final List<List<ThermalMap.Point>> commands = ThermalMap.readStraight(strings);
         ThermalMap tm = new ThermalMap(commands);
-        assertEquals(5, tm.countOverLappedPoints());
+        assertEquals(7644, tm.countOverLappedPoints());
+    }
+
+    @Test
+    public void inputTwoTest(){
+        final List<String> strings = Util.readStrings("/day5/input1.txt");
+        final List<List<ThermalMap.Point>> points = ThermalMap.readAll(strings);
+        ThermalMap tm = new ThermalMap(points);
+        assertEquals(7644, tm.countOverLappedPoints());
     }
 }
